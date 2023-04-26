d3e29e1ee5113a7703aa56c93dea501c47cae7f2
Billy_Joe <williamjcollier4@gmail.com>
2023-04-26 14:49:07 +0800

    added 2 webpack plugins

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
@@ -5,8 +7,38 @@ module.exports = {
 	images: {
 		domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
 		loader: 'cloudinary',
-		path: 'https://res.cloudinary.com/dhqnraex6/image/upload/', 
+		path: 'https://res.cloudinary.com/dhqnraex6/image/upload/',
 	},
 	basePath: isProd ? '/villa-vault' : '',
 	assetPrefix: isProd ? '/villa-vault/' : '',
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.plugins.push(
+				new CompressionPlugin({
+					algorithm: 'gzip',
+					test: /\.(js|css|html|svg)$/,
+					threshold: 8192,
+					minRatio: 0.8,
+				})
+			)
+		}
+		config.optimization.minimize = true
+		config.optimization.minimizer = [
+			new TerserPlugin({
+				terserOptions: {
+					compress: {
+						comparisons: false,
+						dead_code: true,
+						drop_debugger: true,
+						drop_console: true,
+					},
+					output: {
+						comments: false,
+					},
+				},
+				extractComments: false,
+			}),
+		]
+		return config
+	},
 }
