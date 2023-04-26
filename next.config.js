a6933fbdbe64f5834bf3dfa6e0dfda8c25b6e50c
Billy_Joe <williamjcollier4@gmail.com>
2023-04-26 12:36:32 +0800

    added .nojekyll

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
 	},
+	basePath: isProd ? '/villa-vault' : '',
+	assetPrefix: isProd ? '/villa-vault/' : '',
 }
