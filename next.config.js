9eead93256ef6e297368e7974059bffba50a19fc
Billy_Joe <williamjcollier4@gmail.com>
2023-04-26 13:00:15 +0800

    update cloudinary next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
 	reactStrictMode: true,
 	images: {
 		domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+		loader: 'cloudinary',
+		path: 'https://res.cloudinary.com/dhqnraex6/image/upload/', 
 	},
 	basePath: isProd ? '/villa-vault' : '',
 	assetPrefix: isProd ? '/villa-vault/' : '',
